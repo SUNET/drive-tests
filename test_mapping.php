c9b2a1ead60916b1523afada95a1a6833eed6fb5
Richard Freitag <freitag@sunet.se>
2023-10-05 12:03:35 +0100

    Quick manual test for the gss mapping file

---
+++ test_mapping.php
@@ -0,0 +1,24 @@
+
+<?php
+$path = 'mappingfile_test.json';
+$jsonString = file_get_contents($path);
+$jsonData = json_decode($jsonString, true);
+
+$users = ['name@sunet.se','first.last@sunet.se', 'first.last@hh.se', 'first.last@shh.se',
+          'name@sub.sunet.se','first.last@sub.sunet.se', 'first.last@sub.hh.se', 'first.last@sub.shh.se',
+          'name@sub.inst.sunet.se','first.last@sub.inst.sunet.se', 'first.last@sub.inst.hh.se', 'first.last@sub.inst.shh.se',
+          'sunet.se@sub.inst.sunet.se','ki.se@sub.inst.sunet.se', 'sub.inst.shh.se@sub.inst.hh.se', 'sub.inst.hh.se@sub.inst.shh.se',
+          'some-user@eduid.se','ki.se@somedomain.edu','ki.se@ki.se.somedomain.edu'];
+
+foreach ($users as &$user)
+{
+    foreach ($jsonData as $regex => $node) {
+        // printf("Testing regex: %s\n", $regex);
+        if (preg_match($regex, $user, $matches) === 1)
+        {
+            printf("%35s: %s\n", $user, $node );
+            break;
+        }
+    }
+}
+?>
