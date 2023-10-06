06cb19274443d7339fbe2aca029c71b1848e51ed
Richard Freitag <freitag@sunet.se>
2023-10-06 13:44:54 +0100

    More edge cases for testing the user mapping

--- test_mapping.php
+++ test_mapping.php
@@ -8,8 +8,9 @@ $users = ['name@sunet.se','first.last@sunet.se', 'first.last@hh.se', 'first.last
           'name@sub.sunet.se','first.last@sub.sunet.se', 'first.last@sub.hh.se', 'first.last@sub.shh.se',
           'name@sub.inst.sunet.se','first.last@sub.inst.sunet.se', 'first.last@sub.inst.hh.se', 'first.last@sub.inst.shh.se',
           'sunet.se@sub.inst.sunet.se','ki.se@sub.inst.sunet.se', 'sub.inst.shh.se@sub.inst.hh.se', 'sub.inst.hh.se@sub.inst.shh.se',
-          '@.shh.se', 'some@.hh.se', 'some@inst.shhs', 'sunet.se',
-          'some-user@eduid.se','ki.se@somedomain.edu','ki.se@ki.se.somedomain.edu'];
+          '@.shh.se', 'some@.hh.se', '@...se', '@...shh.se', '@...sunet.se.', '@...sunet.se...', 'some@inst.shhs', 'sunet.se', '.sunet.se', '.sunet.se.',
+          'some-user@eduid.se','ki.se@somedomain.edu','ki.se@ki.se.somedomain.edu',
+          'someone@antagning.se'];
 
 foreach ($users as &$user)
 {
