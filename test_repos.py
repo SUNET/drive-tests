c0bed74767c9267481e860ad8396c71ddfb44c8a
Richard Freitag <freitag@sunet.se>
2023-10-20 09:30:37 +0100

    Separate repo-tests for private repositories

---
+++ test_repos.py
@@ -0,0 +1,60 @@
+""" Unit tests that can be run against other Sunet Drive repos for cross-verification
+Author: Richard Freitag <freitag@sunet.se>
+"""
+
+import unittest
+import yaml
+
+import sunetdrive
+import os
+import logging
+import filecmp
+
+opsbase='sunet-drive-ops/'
+puppetbase='drive-puppet/'
+
+puppetfile = './drive-puppet/templates/application/mappingfile-' + os.environ.get('DriveTestTarget') + '.json.erb'
+referencefile = './mappingfile-' + os.environ.get('DriveTestTarget') + '.json'
+
+class TestRepos(unittest.TestCase):
+    logger = logging.getLogger(__name__)
+    logging.basicConfig(format = '%(asctime)s - %(module)s.%(funcName)s - %(levelname)s: %(message)s',
+                    datefmt = '%Y-%m-%d %H:%M:%S', level = logging.INFO)
+
+    def test_logger(self):
+        self.logger.info(f'self.logger.info test_logger')
+        pass
+
+    # Ensure that the mapping files are the same as the reference files
+    def test_mappingfiles(self):
+        self.logger.info(f'{self._testMethodName}')
+        drv = sunetdrive.TestTarget()
+        self.assertTrue(filecmp.cmp(puppetfile, referencefile))
+
+    def test_allnodes_tested(self):
+        self.logger.info(f'{self._testMethodName}')
+        drv = sunetdrive.TestTarget()
+        # print(len(drv.fullnodes))
+
+        testMissing = False
+        testWrongNode = False
+        globalconfigfile = opsbase + "/global/overlay/etc/hiera/data/common.yaml"
+        with open(globalconfigfile, "r") as stream:
+            data=yaml.safe_load(stream)
+            allnodes=data['fullnodes'] + data['singlenodes']
+
+            for node in allnodes:
+                if node not in drv.fullnodes:
+                    print(f'{node} in common.yaml but not tested')
+
+            for node in drv.fullnodes:
+                if node not in allnodes:
+                    print(f'{node} in tests but not in common.yaml')
+
+
+        self.assertFalse(testMissing)
+
+if __name__ == '__main__':
+    import xmlrunner
+    # unittest.main()
+    unittest.main(testRunner=xmlrunner.XMLTestRunner(output='test-reports'))
--- test_tests.py
+++ test_tests.py
@@ -11,10 +11,6 @@ import logging
 import filecmp
 
 opsbase='sunet-drive-ops/'
-puppetbase='drive-puppet/'
-
-puppetfile = './drive-puppet/templates/application/mappingfile-' + os.environ.get('DriveTestTarget') + '.json.erb'
-referencefile = './mappingfile-' + os.environ.get('DriveTestTarget') + '.json'
 
 class TestTests(unittest.TestCase):
     logger = logging.getLogger(__name__)
@@ -24,12 +20,6 @@ class TestTests(unittest.TestCase):
     def test_logger(self):
         self.logger.info(f'self.logger.info test_logger')
         pass
-
-    # Ensure that the mapping files are the same as the reference files
-    def test_mappingfiles(self):
-        self.logger.info(f'{self._testMethodName}')
-        drv = sunetdrive.TestTarget()
-        self.assertTrue(filecmp.cmp(puppetfile, referencefile))
 
     def test_allnodes_tested(self):
         self.logger.info(f'{self._testMethodName}')
