192a115ca3ca30ca56e3a207940c17977accd2bb
Richard Freitag <freitag@sunet.se>
2024-02-22 17:43:00 +0000

    Lean options for chrome testing

--- test_collabora_selenium.py
+++ test_collabora_selenium.py
@@ -71,7 +71,10 @@ class TestCollaboraSelenium(unittest.TestCase):
 
     try:
         options = Options()
-        # options.add_argument("--headless")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--disable-gpu")
+        options.add_argument("--disable-extensions")
         driver = webdriver.Chrome(options=options)
         g_driver=driver
     except:
