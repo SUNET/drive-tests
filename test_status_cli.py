7fefbf3fe5d12ce8b1b4179199ce0709756c901a
Richard Freitag <freitag@sunet.se>
2024-06-18 14:47:15 +0100

    Add status.php test for individual nodes

--- test_status_cli.py
+++ test_status_cli.py
@@ -71,7 +71,55 @@ class FrontendStatusInfo(threading.Thread):
         testThreadsRunning -= 1
         logger.info(f'FrontendStatusInfo threads remaining: {testThreadsRunning}')
 
-class Status(threading.Thread):
+class NodeStatusInfo(threading.Thread):
+    def __init__(self, node, TestStatus):
+        threading.Thread.__init__(self)
+        self.node = node
+        self.TestStatus = TestStatus
+
+    def run(self):
+        global testThreadsRunning
+        global logger
+        global expectedResults
+        global g_failedNodes
+        testThreadsRunning += 1
+        drv = sunetnextcloud.TestTarget()
+        logger.info(f'NodeStatusInfo thread {testThreadsRunning} started for node {self.node}')
+
+        x = range(1,4)
+        for i in x:
+            url = drv.get_node_status_url(self.node, i)
+            try:
+                logger.info(f'Getting status from: {url}')
+                r =requests.get(url, timeout=g_requestTimeout, verify=False)
+            except:
+                logger.error(f'Error getting data from {self.node}')
+                testThreadsRunning -= 1
+                return
+            try:
+                j = json.loads(r.text)
+                self.TestStatus.assertEqual(j["maintenance"], expectedResults[drv.target]['status']['maintenance'])
+                self.TestStatus.assertEqual(j["needsDbUpgrade"], expectedResults[drv.target]['status']['needsDbUpgrade'])
+                self.TestStatus.assertEqual(j["version"], expectedResults[drv.target]['status']['version'])
+                self.TestStatus.assertEqual(j["versionstring"], expectedResults[drv.target]['status']['versionstring'])
+                self.TestStatus.assertEqual(j["edition"], expectedResults[drv.target]['status']['edition'])
+                # self.assertEqual(j["productname"], statusResult.productname)
+                self.TestStatus.assertEqual(j["extendedSupport"], expectedResults[drv.target]['status']['extendedSupport'])
+                logger.info(f'Status information tested: {url}')
+            except:
+                g_failedNodes.append(url)
+                logger.info(f'No valid JSON reply received for {url}')
+                testThreadsRunning -= 1
+                logger.info(r.text)
+                self.TestStatus.assertTrue(False)
+                return
+
+        logger.info(f'Status thread done for node {url}')
+        testThreadsRunning -= 1
+        logger.info(f'NodeStatusInfo threads remaining: {testThreadsRunning}')
+
+# Test frontend status for code 200, no content check
+class FrontentStatus(threading.Thread):
     def __init__(self, url, TestStatus):
         threading.Thread.__init__(self)
         self.url = url
@@ -145,19 +193,44 @@ class TestStatus(unittest.TestCase):
         self.assertEqual(j["extendedSupport"], expectedResults[drv.target]['status_gss']['extendedSupport'])
         logger.info(f'GSS Status information tested')
 
-    def test_status(self):
+    def test_node_statusinfo_gss(self):
+        global logger
+        global expectedResults
+        logger.info(f'TestID: {self._testMethodName}')
+        drv = sunetnextcloud.TestTarget()
+        if drv.testgss == False:
+            logger.info('Not testing gss')
+            return
+
+        x = range(1,4)
+        for i in x:
+            url=drv.get_gss_url() + "/status.php"
+            url=url.replace('https://','https://gss' + str(i) + ".")
+            logger.info(f'{self._testMethodName}: {url}')
+            r =requests.get(url, timeout=g_requestTimeout, verify=False)
+            j = json.loads(r.text)
+
+            self.assertEqual(j["maintenance"], expectedResults[drv.target]['status_gss']['maintenance']) 
+            self.assertEqual(j["needsDbUpgrade"], expectedResults[drv.target]['status_gss']['needsDbUpgrade'])
+            self.assertEqual(j["version"], expectedResults[drv.target]['status_gss']['version'])
+            self.assertEqual(j["versionstring"], expectedResults[drv.target]['status_gss']['versionstring'])
+            self.assertEqual(j["edition"], expectedResults[drv.target]['status_gss']['edition'])
+            self.assertEqual(j["extendedSupport"], expectedResults[drv.target]['status_gss']['extendedSupport'])
+            logger.info(f'GSS Status information tested')
+
+    def test_frontend_status(self):
         drv = sunetnextcloud.TestTarget()
         for url in drv.get_allnode_status_urls():
             with self.subTest(myurl=url):
                 logger.info(f'TestID: {url}')
-                statusThread = Status(url, self)
+                statusThread = FrontentStatus(url, self)
                 statusThread.start()
 
         while(testThreadsRunning > 0):
             time.sleep(1)
 
         if len(g_failedNodes) > 0:
-            logger.error(f'Status test failed for {len(g_failedNodes)} of {len(drv.allnodes)} nodes:')
+            logger.error(f'Frontend status test failed for {len(g_failedNodes)} of {len(drv.allnodes)} nodes:')
             for node in g_failedNodes:
                 logger.error(f'   {node}')
             self.assertTrue(False)
@@ -176,6 +249,24 @@ class TestStatus(unittest.TestCase):
 
         if len(g_failedNodes) > 0:
             logger.error(f'FrontendStatusInfo test failed for {len(g_failedNodes)} of {len(drv.allnodes)} nodes:')
+            for node in g_failedNodes:
+                logger.error(f'   {node}')
+            self.assertTrue(False)
+
+# Test status infor content for all individual loadbalanced nodes
+    def test_node_statusinfo(self):
+        drv = sunetnextcloud.TestTarget()
+        for node in expectedResults['global']['redundantnodes']:
+            with self.subTest(myurl=node):
+                logger.info(f'TestID: {node}')
+                statusInfoThread = NodeStatusInfo(node, self)
+                statusInfoThread.start()
+
+        while(testThreadsRunning > 0):
+            time.sleep(1)
+
+        if len(g_failedNodes) > 0:
+            logger.error(f'NodeStatusInfo test failed for {len(g_failedNodes)} of {len(drv.allnodes)} nodes:')
             for node in g_failedNodes:
                 logger.error(f'   {node}')
             self.assertTrue(False)
