248054dee03886c3fba641651e41ce6c1324cc97
Richard Freitag <freitag@sunet.se>
2024-12-15 16:30:17 +0000

    Remove gss from loginhelper

--- driveweblogin.py
+++ driveweblogin.py
@@ -11,7 +11,7 @@ g_urlPrefix = ''
 parser = argparse.ArgumentParser(description="Open Sunet Drive direct login pages in Firefox or Chrome",
                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('browser', choices = ['firefox', 'chrome'], help="Browser - firefox or chrome")
-parser.add_argument('customer', help="Customer - gss, <node>, all")
+parser.add_argument('customer', help="Customer - <node>, all")
 parser.add_argument('environment', choices = ['test', 'prod'], help="Environment")
 args = parser.parse_args()
 config = vars(args)
@@ -24,11 +24,13 @@ if args.browser == 'firefox':
 elif args.browser == 'chrome':
     g_exec += g_chromeCmd
 
-if args.customer == 'gss':
-    g_exec += g_execPrefix + "'" + drv.get_node_login_url('gss') + "'"
-elif args.customer == 'all':
-    g_exec += g_execPrefix + "'" + drv.get_node_login_url('gss') + "' "
+if args.customer == 'all':
+    for node in drv.allnodes:
+        print(f'{node}')
+        g_exec += g_execPrefix + "'" + drv.get_node_login_url(node) + "' "
+elif args.customer == 'full':
     for node in drv.fullnodes:
+        print(f'{node}')
         g_exec += g_execPrefix + "'" + drv.get_node_login_url(node) + "' "
 
 else:
