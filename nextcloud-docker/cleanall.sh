a58bb27cc8c1e7ecf75160f867c6efa52a1b8b6b
Richard Freitag <freitag@sunet.se>
2025-01-09 19:22:23 +0000

    Clean all in makefile

--- nextcloud-docker/cleanall.sh
+++ nextcloud-docker/cleanall.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+
+echo Prune system and voumes and remove containers, images and volumes in 5s. Press Ctrl-C to cancel.
+for i in {5..1}; do echo $i & sleep 1; done
+
 docker rm -vf $(docker ps -aq)
 docker rmi -f $(docker images -aq)
-docker system prune
-docker volume prune
+docker system prune -f
+docker volume prune -f
 docker volume rm nextcloud-docker_db
 docker volume rm nextcloud-docker_nextcloud
 docker volume rm nextcloud-docker_data1-1
