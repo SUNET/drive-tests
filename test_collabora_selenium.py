be4255aa18067633e599c186e801330ddfd4f06d
Richard Freitag <freitag@sunet.se>
2025-03-04 12:45:35 +0000

    Use Nextcloud version in html test reports

--- test_collabora_selenium.py
+++ test_collabora_selenium.py
@@ -869,4 +869,4 @@ if __name__ == '__main__':
     if g_drv.testrunner == 'xml':
         unittest.main(testRunner=xmlrunner.XMLTestRunner(output='test-reports'))
     else:
-        unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner(output='test-reports-html', combine_reports=True, report_name="nextcloud-collabora", add_timestamp=False))
+        unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner(output='test-reports-html', combine_reports=True, report_name=f"nextcloud-{drv.expectedResults[drv.target][status][version]}-collabora", add_timestamp=False))
