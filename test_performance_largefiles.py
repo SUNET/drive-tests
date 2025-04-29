89e80af582439b57ca53ab00ff5d0a98dbe06136
Richard Freitag <freitag@sunet.se>
2025-04-29 10:26:54 +0100

    Preparation for large file test

---
+++ test_performance_largefiles.py
@@ -0,0 +1,186 @@
+""" Testing WebDavPerformance functions for Sunet Drive
+Author: Richard Freitag <freitag@sunet.se>
+"""
+
+import unittest
+import tempfile
+import requests
+from requests.auth import HTTPBasicAuth
+import json
+import os
+from pathlib import Path
+import subprocess
+import shutil
+from webdav3.client import Client
+from webdav3.exceptions import WebDavException
+import logging
+import threading
+import time
+import sys
+from datetime import datetime
+import xmlrunner
+
+import sunetnextcloud
+
+g_maxCheck = 10
+g_WebDavPerformance_timeout = 30
+g_testFolder = 'WebDavPerformanceTest'
+g_stressTestFolder = 'WebDavPerformanceStressTest'
+g_sharedTestFolder = 'SharedFolder'
+g_personalBucket = 'selenium-personal'
+g_systemBucket = 'selenium-system'
+g_filename=datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+g_davPerformanceResults = []
+g_testPassed = {}
+g_testThreadsRunning = 0
+ocsheaders = { "OCS-APIRequest" : "true" } 
+logger = logging.getLogger(__name__)
+logging.basicConfig(format = '%(asctime)s - %(module)s.%(funcName)s - %(levelname)s: %(message)s',
+                datefmt = '%Y-%m-%d %H:%M:%S', level = logging.INFO)
+
+KB = 1024
+MB = 1024 * KB
+GB = 1024 * MB
+fileSizes=[1,4,8,29] # Only GB, larger than 1
+fileNames=[] # Array with the files 
+useTmpFolder = True # As a default, use the temp folder, alternatively use home folder of user that is testing
+
+def deleteTestData():
+    for fileSize in fileSizes:
+        filename = f'{str(fileSize)}G.bin'
+        pathname = f'{tempfile.gettempdir()}/{filename}'            # Check if the file exists in the temp directory
+        if Path(pathname).exists():
+            logger.info(f'Removing file {pathname}')
+            os.remove(pathname)
+        pathname = f'{Path.home()}/{filename}'                      # Check if the file exists in the home directory
+        if Path(pathname).exists():
+            logger.info(f'Removing file {pathname}')
+            os.remove(pathname)
+
+def checkTestData():
+    for fileSize in fileSizes:
+        filename = f'{str(fileSize)}G.bin'
+        pathname = f'{tempfile.gettempdir()}/{filename}'            # Check if the file exists in the temp directory
+        if Path(pathname).exists():
+            existingFileSize = Path(pathname).stat().st_size / GB
+            if existingFileSize == fileSize:
+                logger.info(f'File {pathname} exists with correct file size {fileSize}')
+            else:
+                logger.warning(f'File size for {pathname} does not match {existingFileSize} != {fileSize}')
+                return False
+
+        pathname = f'{Path.home()}/{filename}'                      # Check if the file exists in the home directory
+        if Path(pathname).exists():
+            existingFileSize = Path(pathname).stat().st_size / GB
+            if existingFileSize == fileSize:
+                logger.info(f'File {pathname} exists with correct file size {fileSize}')
+            else:
+                logger.warning(f'File size for {pathname} does not match {existingFileSize} != {fileSize}')
+                return False
+    return True
+
+def generateTestData():
+    # Check if we have enought space in the temp directory; we assume that check and/or delete was executed before this
+    tmpFree = shutil.disk_usage('/tmp').free / GB
+    logger.info(f'/tmp has {tmpFree:.2f} GB free')
+    if tmpFree > 42:
+        useTmp = True
+        logger.info(f'Using /tmp for temporary files')
+    else:
+        homeFree = shutil.disk_usage(Path.home()).free /GB
+        logger.info(f'{Path.home()} has {homeFree:.2f} GB free')
+        if homeFree < 42:
+            logger.error(f'Not enough space in /tmp or {Path.home}')
+            TestLargeFilePerformance.assertTrue(False)
+        logger.info(f'Using home directory {Path.home()}')
+        useTmp = False
+
+    for fileSize in fileSizes:
+        filename = f'{str(fileSize)}G.bin'
+        if useTmp:
+            pathname = f'{tempfile.gettempdir()}/{filename}'
+        else: # use home folder
+            pathname = f'{Path.home()}/{filename}'
+
+        # Check if the file already exists
+        makeFiles = True
+        if Path(pathname).exists():
+            logger.info(f'File {pathname} already exists')
+            existingFileSize = Path(pathname).stat().st_size / GB
+            if existingFileSize != fileSize:
+                logger.warning(f'File sizes do not match, recreating {existingFileSize} != {fileSize}')
+                makeFiles = True
+            else:
+                logger.info(f'File sizes match, no need to recreate {existingFileSize} == {fileSize}')
+                makeFiles = False
+
+        if makeFiles:
+            logger.info(f'Generating file {pathname}')
+            cmd=f'head -c {fileSize}G /dev/urandom > {pathname}'
+            logger.info(f'Running subprocess {cmd}')
+            os.system(cmd)
+        # subprocess.run(cmd, stdout = subprocess.DEVNULL)        
+
+def excepthook(args):
+    logger.error(f'Threading exception: {args}')
+    decreaseUploadCount()
+    sys.excepthook(*sys.exc_info())
+
+threading.excepthook = excepthook
+
+def decreaseUploadCount():
+    global g_testThreadsRunning
+    g_testThreadsRunning -= 1
+
+def webdavUpload(client, local_path, remote_path):
+    global logger, g_testThreadsRunning
+    logger.info(f'Upload {local_path}')
+    if client == None:
+        logger.error(f'No client provided')
+        decreaseUploadCount()
+        return
+    if local_path == None:
+        logger.error(f'No filename provided')
+        decreaseUploadCount()
+        return
+    if remote_path == None:
+        logger.error(f'No target filename provided')
+        decreaseUploadCount()
+        return
+    try:
+        client.upload_sync(remote_path=remote_path,local_path=local_path)
+    except Exception as error:
+        logger.error(f'Error uploading {remote_path}:{error}')
+        decreaseUploadCount()
+        return
+
+    decreaseUploadCount()
+
+def webdavClean(client, filename):
+    global logger, g_testThreadsRunning
+    if client == None:
+        logger.error(f'No client provided')
+        return
+    if filename == None:
+        logger.error(f'No filename provided')
+        return
+    logger.info(f'Cleaning {filename}')
+    client.clean(filename)
+
+class TestLargeFilePerformance(unittest.TestCase):
+    def test_logger(self):
+        global logger
+        logger.info(f'TestID: {self._testMethodName}')
+        pass
+
+    def test_testdata(self):
+        if checkTestData() == False:
+            logger.warning(f'File size check failed, regenerating test data')
+            deleteTestData()
+            generateTestData()
+
+        self.assertTrue(checkTestData())
+        pass
+
+if __name__ == '__main__':
+    unittest.main(testRunner=xmlrunner.XMLTestRunner(output='test-reports'))
