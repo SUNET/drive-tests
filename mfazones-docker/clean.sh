ce782189408c0dc2f5c740d85cdbe0eed712f527
Richard Freitag <freitag@sunet.se>
2025-08-02 20:21:46 +0100

    Remove mfa zones docker

--- mfazones-docker/clean.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-rm .env.yaml
-docker exec -u www-data nextcloud /bin/bash -c "/var/www/html/occ twofactorauth:disable admin totp"
-docker exec -u www-data nextcloud /bin/bash -c "/var/www/html/occ twofactorauth:disable mfauser totp"
