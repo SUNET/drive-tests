9629c7378c458d50112a7980f3976cfc38fed780
Richard Freitag <freitag@sunet.se>
2025-08-12 14:31:01 +0100

    Ruff fix F401 (unused imports)

--- test_search_selenium.py
+++ test_search_selenium.py
@@ -15,7 +15,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.chrome.options import Options
 import os
 import logging
