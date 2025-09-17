681344a13f190b7ea93b39c20f20dd19886f2e04
Richard Freitag <freitag@sunet.se>
2025-09-17 11:40:27 +0100

    30s request timeout instead of 10s for status test

--- test_status_cli.py
+++ test_status_cli.py
@@ -21,7 +21,7 @@ expectedResults = drv.expectedResults
 
 testThreadsRunning = 0
 g_failedNodes = []
-g_requestTimeout=10
+g_requestTimeout=30
 logger = logging.getLogger(__name__)
 logging.basicConfig(format = '%(asctime)s - %(module)s.%(funcName)s - %(levelname)s: %(message)s',
                 datefmt = '%Y-%m-%d %H:%M:%S', level = logging.INFO)
