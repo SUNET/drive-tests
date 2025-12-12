b269da72f16f319067f9897c30e2cfbbb4f6da14
Richard Freitag <freitag@sunet.se>
2025-12-12 16:59:47 +0000

    Remove unnecessary clean file

--- nextcloud-docker/cleangentle.sh
+++ nextcloud-docker/cleangentle.sh
@@ -44,5 +44,3 @@ docker volume rm nextcloud-docker_data3-1
 docker volume rm nextcloud-docker_data3-2
 docker volume rm nextcloud-docker_data4-1
 docker volume rm nextcloud-docker_data4-2
-
-rmdir /tmp/localhost.pem
