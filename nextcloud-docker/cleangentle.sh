8c7583182a535a5f6e916746ae1955c36d8c60b7
Richard Freitag <freitag@sunet.se>
2025-12-15 13:19:54 +0000

    WIP for Nextcloud research cloud deployment

--- nextcloud-docker/cleangentle.sh
+++ nextcloud-docker/cleangentle.sh
@@ -14,19 +14,19 @@ docker kill nextcloud-local 2&> /dev/null || true
 docker kill nextcloud-local-db 2&> /dev/null || true
 docker kill nextcloud-local-whiteboard 2&> /dev/null || true
 docker kill nextcloud-local-minio 2&> /dev/null || true
-docker kill nextcloud-local-minio1-1 2&> /dev/null || true
-docker kill nextcloud-local-minio2-1 2&> /dev/null || true
-docker kill nextcloud-local-minio3-1 2&> /dev/null || true
-docker kill nextcloud-local-minio4-1 2&> /dev/null || true
+docker kill nextcloud-local-minio1 2&> /dev/null || true
+docker kill nextcloud-local-minio2 2&> /dev/null || true
+docker kill nextcloud-local-minio3 2&> /dev/null || true
+docker kill nextcloud-local-minio4 2&> /dev/null || true
 
 docker rm -vf nextcloud-local
 docker rm -vf nextcloud-local-db
 docker rm -vf nextcloud-whiteboard
 docker rm -vf nextcloud-local-nginx
-docker rm -vf nextcloud-local-minio1-1
-docker rm -vf nextcloud-local-minio2-1
-docker rm -vf nextcloud-local-minio3-1
-docker rm -vf nextcloud-local-minio4-1
+docker rm -vf nextcloud-local-minio1
+docker rm -vf nextcloud-local-minio2
+docker rm -vf nextcloud-local-minio3
+docker rm -vf nextcloud-local-minio4
 
 docker rmi -f quay.io/minio/minio
 docker rmi -f nextcloud
@@ -36,11 +36,11 @@ docker rmi -f nginx
 
 docker volume rm nextcloud-docker_db
 docker volume rm nextcloud-docker_nextcloud
-docker volume rm nextcloud-docker_data1-1
-docker volume rm nextcloud-docker_data1-2
-docker volume rm nextcloud-docker_data2-1
-docker volume rm nextcloud-docker_data2-2
-docker volume rm nextcloud-docker_data3-1
-docker volume rm nextcloud-docker_data3-2
-docker volume rm nextcloud-docker_data4-1
-docker volume rm nextcloud-docker_data4-2
+docker volume rm nextcloud-docker_data1
+docker volume rm nextcloud-docker_data1
+docker volume rm nextcloud-docker_data2
+docker volume rm nextcloud-docker_data2
+docker volume rm nextcloud-docker_data3
+docker volume rm nextcloud-docker_data3
+docker volume rm nextcloud-docker_data4
+docker volume rm nextcloud-docker_data4
